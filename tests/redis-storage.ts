1d71c369e1e813852297888bee76ad345bb77d30
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-02-25 00:26:07 +0200

    Fixing the test of redis storage

--- tests/redis-storage.ts
+++ tests/redis-storage.ts
@@ -3,26 +3,25 @@ import { RedisStorage } from '../src/storage-managers/redis-storage';
 const storage = new RedisStorage({
     host: '127.0.0.1',
     port: 6379
-})
-const client = storage.client;
+});
+
 describe('Testing databases', async function() {
-    this.timeout(1000 * 60 * 60);
     it('Get/Set keys', async () => {
-        client.set('example', 'value');
-        const value: any = await client.get('example');
+        await storage.client.set('example', 'value');
+        const value: any = await storage.client.get('example');
         expect(value).to.eql('value', 'Value of the key');
-        console.log(`${new Date().toLocaleDateString('GB')}: Get/Set keys is done.`);
+        console.log(`${new Date().toLocaleString('GB')}: Get/Set keys is done.`);
     });
     it('getKeys function', async () => {
-        client.set('example1', 'value1');
-        client.set('example2', 'value2');
-        client.set('example3', 'value3');
-        const object = storage.getKeys(['example1', 'example2']);
+        await storage.client.set('example1', 'value1');
+        await storage.client.set('example2', 'value2');
+        await storage.client.set('example3', 'value3');
+        const object = await storage.getKeys(['example1', 'example2']);
         const expectedObject = {
             'example1': 'value1',
             'example2': 'value2'
-        }
+        };
         expect(expectedObject).to.eql(object, 'Equality of expected and actual objects');
-        console.log(`${new Date().toLocaleDateString('GB')}: getKeys function is done.`);
+        console.log(`${new Date().toLocaleString('GB')}: getKeys function is done.`);
     });
 });
