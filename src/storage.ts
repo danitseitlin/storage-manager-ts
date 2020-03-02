0401f297afa7ef5095b086e2c3a143d6e8ea2f9d
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-03-03 00:36:16 +0200

    Aligning the storage.ts file

--- src/storage.ts
+++ src/storage.ts
@@ -1,29 +1,27 @@
-
-    /**
-     * A function that filters keys by regular expression
-     * @param regex The regular expression to filter with, could be just a text, use * to mention starts with
-     */
-    export function filterKeys(regex: string, allKeys: string[]): string[] {
-        let keys: string[] = [];
-        for(let i = 0; i < allKeys.length; i++) {
-            const key: string = allKeys[i];
-            if(compareKeys(regex, key)) {
-                keys.push(key);
-            }
+/**
+ * A function that filters keys by regular expression
+ * @param regex The regular expression to filter with, could be just a text, use * to mention starts with
+ */
+export function filterKeys(regex: string, allKeys: string[]): string[] {
+    let keys: string[] = [];
+    for(let i = 0; i < allKeys.length; i++) {
+        const key: string = allKeys[i];
+        if(compareKeys(regex, key)) {
+            keys.push(key);
         }
-        return keys;
-    }//xx
-
-    /**
-     * Returning if the keys match with a regex, * will indicate that an a key starts with the regex after the *
-     * @param regex The regex to filter the key with
-     * @param key The key to filter with the regex
-     */
-    export function compareKeys(regex: string, key: string): boolean {
-        let comparison: boolean = false;
-        //If key starts with * we will make sure the key starts with the given regex value
-        if(regex[0] === "*") comparison = key.startsWith(regex.substr(1));
-        //Otherwise, normal comparison
-        else comparison = key.includes(regex);
-        return comparison;//s
     }
+    return keys;
+}
+/**
+ * Returning if the keys match with a regex, * will indicate that an a key starts with the regex after the *
+ * @param regex The regex to filter the key with
+ * @param key The key to filter with the regex
+ */
+export function compareKeys(regex: string, key: string): boolean {
+    let comparison: boolean = false;
+    //If key starts with * we will make sure the key starts with the given regex value
+    if(regex[0] === "*") comparison = key.startsWith(regex.substr(1));
+    //Otherwise, normal comparison
+    else comparison = key.includes(regex);
+    return comparison;
+}
