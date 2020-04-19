fdc3f74ca60b3a2c9b9037fcbfb82440f1d122c5
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-04-19 21:52:43 +0300

    Small additional aligning

---
+++ index.ts
@@ -0,0 +1,2 @@
+export { LocalStorage } from './src/storage-managers/local-storage';
+export { RedisStorage } from './src/storage-managers/redis-storage'
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { LocalStorage } from './storage-managers/local-storage';
-export { RedisStorage } from './storage-managers/redis-storage'
