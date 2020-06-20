66b816e5498675fa0e9bb5dd2e8c2595ca9713cc
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-06-21 00:55:11 +0300

    Fixing the exporting issues and redesigning the repo

---
+++ index.ts
@@ -0,0 +1,39 @@
+export { LocalStorage } from './local-storage';
+
+/**
+ * Returning if the keys match with a regex, * will indicate that an a key starts with the regex after the *
+ * @param regex The regex to filter the key with
+ * @param keys The keys to filter
+ */
+export function filterKeys(keys: string[], regex: regexComparison): string[] {
+    const filteredKeys: string[] = [];
+    for(const key of keys) {
+        if(filterKey(regex, key)) {
+            filteredKeys.push(key);
+        }
+    }
+    return filteredKeys;
+}
+/**
+ * Returning if the keys match with a regex, * will indicate that an a key starts with the regex after the *
+ * @param regex The regex to filter the key with
+ * @param key The key to filter with the regex
+ */
+export function filterKey(regex: regexComparison, key: string): boolean {    
+    let comparison = false;
+    if(regex.startsWith !== undefined && regex.endsWith === undefined) comparison = key.startsWith(regex.startsWith);
+    else if(regex.endsWith !== undefined && regex.startsWith === undefined) comparison = key.endsWith(regex.endsWith);
+    else if(regex.startsWith !== undefined && regex.endsWith !== undefined) 
+        comparison = key.startsWith(regex.startsWith) && key.endsWith(regex.endsWith);
+    return comparison;
+}
+
+/**
+ * The regex filtering options
+ * @param startsWith a regex for a string at the start
+ * @param endsWith a regex for a string at the end
+ */
+export interface regexComparison {
+    startsWith?: string,
+    endsWith?: string
+}
--- tests/local-storage.ts
+++ tests/local-storage.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { LocalStorage } from '../src/storage-managers/local-storage';
+import { LocalStorage } from '../local-storage';
+import { filterKeys,  } from '../index'
 
 const storage = new LocalStorage();
 describe('Testing databases', async function() {
@@ -17,15 +18,15 @@ describe('Testing databases', async function() {
     });
 
     it('filterKeys function', async () => {
-        let keys = storage.filterKeys({start: 'ex'});
+        let keys = filterKeys(Object.keys(storage.getKeys()),{startsWith: 'ex'});
         expect(keys.length).to.eql(1, 'Keys count');
-        keys = storage.filterKeys({start: 'xxxx'});
+        keys = filterKeys(Object.keys(storage.getKeys()),{startsWith: 'xxxx'});
         expect(keys.length).to.eql(0, 'Keys count');
     });
 
     it('getKeysByList function', async () => {
         storage.set('example2', 'value');
-        let keys = storage.getKeysByList(['example', 'example2']);
+        let keys = storage.convertKeyListToJSON(['example', 'example2']);
         expect(Object.keys(keys).length).to.eql(2, 'Keys count');
         expect(keys['example2']).to.eql('value', `key value of example2`);
     });
